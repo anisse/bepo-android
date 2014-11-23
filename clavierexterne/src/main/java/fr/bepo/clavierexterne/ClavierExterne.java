e9c222b6b1d6c74924ddb75ddab7fc557497c383
Anisse Astier <anisse@astier.eu>
2014-11-23 22:58:13 +0100

    Initial version of bépo keymap

---
+++ clavierexterne/src/main/java/fr/bepo/clavierexterne/ClavierExterne.java
@@ -0,0 +1,11 @@
+package fr.bepo.clavierexterne;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class ClavierExterne extends BroadcastReceiver{
+    @Override
+    public void onReceive(Context context, Intent intent) {
+    }
+}
